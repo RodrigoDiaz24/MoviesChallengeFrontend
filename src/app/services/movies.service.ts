f5954bc9f9b7823ac982559ec6580debfa674d95
Rodrigo Diaz <rodrigo_diaz24@hotmail.com>
2025-09-29 10:21:12 -0300

    Ignored Karma testing

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -16,7 +16,7 @@ export interface Movie {
   providedIn: 'root'
 })
 export class MoviesService {
-  private apiUrl = 'moviesearch-ekcybggphmczdehv.westeurope-01.azurewebsites.net/api/movies'; 
+  private apiUrl = 'https://moviesearch-ekcybggphmczdehv.westeurope-01.azurewebsites.net/api/movies'; 
 
   constructor(private http: HttpClient) {}
 
